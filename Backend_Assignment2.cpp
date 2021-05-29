52afa5f069aa4e9ec6786f40ff4508ea1d5269d8
akashvermansit <85019170+akashvermansit@users.noreply.github.com>
2021-05-29 19:31:45 +0530

    Add files via upload

---
+++ Backend_Assignment2.cpp
@@ -0,0 +1,346 @@
+// C++ implementation of the approach
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 1000000
+#define NM 100001
+
+// Adjacency list representation of tree
+vector<int> adj[NM]; 
+set<int> sts;
+map<pair<int,int>,int> mps,mpe;
+  
+// Visited array to keep track visited 
+// nodes on tour
+int vis[NM];
+int dis[NM]; 
+int arr[2*NM] = {0};
+int n = 0;
+int x;
+int ans = 0;
+  
+// Array to store Euler Tour
+int Euler[2 * NM]; 
+  
+// Function to add edges to tree
+void add_edge(int u, int v)
+{
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+  
+// Function to store Euler Tour of tree
+void eulerTree(int u, int &indx)
+{
+    vis[u] = 1;
+    if(sts.find(u)!=sts.end())
+        arr[indx] = dis[u];
+    else
+        arr[indx] = -1000000;
+    //cout<<u<<" "<<arr[indx]<<endl;
+    Euler[indx++] = u;
+    n = max(n,indx);
+    int st = indx;
+    for (auto it : adj[u]) {
+        if (!vis[it]) {
+            eulerTree(it, indx);
+            if(sts.find(u)!=sts.end())
+                arr[indx] = dis[u];
+            else
+                arr[indx] = -1000000;
+            //cout<<u<<" "<<arr[indx]<<endl;
+            mps[{u,it}] = st;
+            mpe[{u,it}] = indx-1;
+            
+            Euler[indx++] = u;
+            st =indx;
+            n = max(n,indx);
+        }
+    }
+}
+
+void dfs(int u,int pd){
+    vis[u] = 1;
+    dis[u] = pd+1;
+    //cout<<u;
+    for(auto it:adj[u]){
+        if(vis[it]!=1){
+            dfs(it,pd+1);
+            //cout<<u;
+        }
+    }
+}
+
+
+  
+// Function to print Euler Tour of tree
+void printEulerTour(int root, int N)
+{
+    int index = 0;  
+    eulerTree(root, index);
+    for (int i = 0; i < (2*N-1); i++)
+        cout << Euler[i] << " ";
+}
+  
+// Ideally, we should not use global variables and large
+// constant-sized arrays, we have done it here for simplicity
+  
+// To store segment tree
+int tree[MAX] = { 0 };
+  
+// To store pending updates
+int lazy[MAX] = { 0 };
+  
+// si -> index of current node in segment tree
+// ss and se -> Starting and ending indexes of
+// elements for which current nodes stores sum
+// us and ue -> starting and ending indexes of update query
+// diff -> which we need to add in the range us to ue
+void updateRangeUtil(int si, int ss, int se, int us,
+                     int ue, int diff)
+{
+    // If lazy value is non-zero for current node of segment
+    // tree, then there are some pending updates. So we need
+    // to make sure that the pending updates are done before
+    // making new updates. Because this value may be used by
+    // parent after recursive calls (See last line of this
+    // function)
+    if (lazy[si] != 0) {
+        // Make pending updates using value stored in lazy
+        // nodes
+        tree[si] += lazy[si];
+  
+        // Checking if it is not leaf node because if
+        // it is leaf node then we cannot go further
+        if (ss != se) {
+            // We can postpone updating children we don't
+            // need their new values now.
+            // Since we are not yet updating children of si,
+            // we need to set lazy flags for the children
+            lazy[si * 2 + 1] += lazy[si];
+            lazy[si * 2 + 2] += lazy[si];
+        }
+  
+        // Set the lazy value for current node as 0 as it
+        // has been updated
+        lazy[si] = 0;
+    }
+  
+    // Out of range
+    if (ss > se || ss > ue || se < us)
+        return;
+  
+    // Current segment is fully in range
+    if (ss >= us && se <= ue) {
+        // Add the difference to current node
+        tree[si] += diff;
+  
+        // Same logic for checking leaf node or not
+        if (ss != se) {
+            // This is where we store values in lazy nodes,
+            // rather than updating the segment tree itelf
+            // Since we don't need these updated values now
+            // we postpone updates by storing values in lazy[]
+            lazy[si * 2 + 1] += diff;
+            lazy[si * 2 + 2] += diff;
+        }
+        return;
+    }
+  
+    // If not completely in range, but overlaps
+    // recur for children
+    int mid = (ss + se) / 2;
+    updateRangeUtil(si * 2 + 1, ss, mid, us, ue, diff);
+    updateRangeUtil(si * 2 + 2, mid + 1, se, us, ue, diff);
+  
+    // And use the result of children calls
+    // to update this node
+    tree[si] = max(tree[si * 2 + 1], tree[si * 2 + 2]);
+}
+  
+// Function to update a range of values in segment
+// tree
+// us and eu -> starting and ending indexes of update query
+// ue -> ending index of update query
+// diff -> which we need to add in the range us to ue
+void updateRange(int n, int us, int ue, int diff)
+{
+    updateRangeUtil(0, 0, n - 1, us, ue, diff);
+}
+  
+// A recursive function to get the sum of values in given
+// a range of the array. The following are the parameters
+// for this function
+// si --> Index of the current node in the segment tree
+// Initially, 0 is passed as root is always at index 0
+// ss & se --> Starting and ending indexes of the
+// segment represented by current node
+// i.e., tree[si]
+// qs & qe --> Starting and ending indexes of query
+// range
+int getSumUtil(int ss, int se, int qs, int qe, int si)
+{
+  
+    // If lazy flag is set for current node of segment tree
+    // then there are some pending updates. So we need to
+    // make sure that the pending updates are done before
+    // processing the sub sum query
+    if (lazy[si] != 0) {
+  
+        // Make pending updates to this node. Note that this
+        // node represents sum of elements in arr[ss..se] and
+        // all these elements must be increased by lazy[si]
+        tree[si] += lazy[si];
+  
+        // Checking if it is not leaf node because if
+        // it is leaf node then we cannot go further
+        if (ss != se) {
+            // Since we are not yet updating children os si,
+            // we need to set lazy values for the children
+            lazy[si * 2 + 1] += lazy[si];
+            lazy[si * 2 + 2] += lazy[si];
+        }
+  
+        // Unset the lazy value for current node as it has
+        // been updated
+        lazy[si] = 0;
+    }
+  
+    // Out of range
+    if (ss > se || ss > qe || se < qs)
+        return 0;
+  
+    // At this point, we are sure that pending lazy updates
+    // are done for current node. So we can return value
+    // (same as it was for a query in our previous post)
+  
+    // If this segment lies in range
+    if (ss >= qs && se <= qe)
+        return tree[si];
+  
+    // If a part of this segment overlaps with the given
+    // range
+    int mid = (ss + se) / 2;
+    return max(getSumUtil(ss, mid, qs, qe, 2 * si + 1),
+               getSumUtil(mid + 1, se, qs, qe, 2 * si + 2));
+}
+  
+// Return sum of elements in range from index qs (query
+// start) to qe (query end). It mainly uses getSumUtil()
+int getSum(int n, int qs, int qe)
+{
+    // Check for erroneous input values
+    if (qs < 0 || qe > n - 1 || qs > qe) {
+        printf("Invalid Input");
+        return -1;
+    }
+  
+    return getSumUtil(0, n - 1, qs, qe, 0);
+}
+  
+// A recursive function that constructs Segment Tree for
+// array[ss..se]. si is index of current node in segment
+// tree st.
+void constructSTUtil(int arr[], int ss, int se, int si)
+{
+    // out of range as ss can never be greater than se
+    if (ss > se)
+        return;
+  
+    // If there is one element in array, store it in
+    // current node of segment tree and return
+    if (ss == se) {
+        tree[si] = arr[ss];
+        return;
+    }
+  
+    // If there are more than one elements, then recur
+    // for left and right subtrees and store the sum
+    // of values in this node
+    int mid = (ss + se) / 2;
+    constructSTUtil(arr, ss, mid, si * 2 + 1);
+    constructSTUtil(arr, mid + 1, se, si * 2 + 2);
+  
+    tree[si] = max(tree[si * 2 + 1], tree[si * 2 + 2]);
+}
+  
+// Function to construct a segment tree from a given array
+// This function allocates memory for segment tree and
+// calls constructSTUtil() to fill the allocated memory
+void constructST(int arr[], int n)
+{
+    // Fill the allocated memory st
+    constructSTUtil(arr, 0, n - 1, 0);
+}
+
+void edfs(int u, int &indx){
+    vis[u] = 1;
+    indx++;
+    int st = indx;
+    int mxd = getSum(n, 0, n-1);
+    //cout<<u<<mxd<<endl;
+    if(mxd<=x)
+        ans++;
+    for (auto it : adj[u]) {
+        if (!vis[it]) {
+            int en = mpe[{u,it}];
+            updateRange(n, st,en, -1);
+            if(st-1>=0){
+                updateRange(n,0,st-1,1);
+            }
+            if(indx+1<n){
+                updateRange(n,en+1,n-1,1);
+            }
+            edfs(it, indx);
+            updateRange(n, st,en, +1);
+            if(st-1>=0){
+                updateRange(n,0,st-1,-1);
+            }
+            if(indx+1<n){
+                updateRange(n,en+1,n-1,-1);
+            }
+            
+            indx++;
+            st = indx;
+        }
+    }
+}
+// Driver code
+int main()
+{
+    
+  
+    int nms,h;
+    cin>>nms>>h>>x;
+    for(int i = 0;i<h;i++){
+        int a;
+        cin>>a;
+        a--;
+        sts.insert(a);
+    }
+    for(int i = 0;i<nms-1;i++){
+        int u,v;
+        cin>>u>>v;
+        //cout<<u<<" "<<v<<endl;
+        u--;
+        v--;
+        add_edge(u,v);
+    }
+    for(int i = 0;i<nms;i++){
+        vis[i] = 0;
+    }
+    dfs(0,-1);
+    for(int i = 0;i<nms;i++){
+        vis[i] = 0;
+    }
+    int ii = 0;
+    eulerTree(0,ii);
+    constructST(arr, n);
+    for(int i = 0;i<nms;i++){
+        vis[i] = 0;
+    }
+    ii = 0;
+    edfs(0,ii);
+  
+    cout<<ans<<endl;
+    return 0;
+}
